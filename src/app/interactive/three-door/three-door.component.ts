002064a56321eaaa5eac45e674dd3c9a0646a5d9
sws <sws@paywith.co.kr>
2021-04-04 09:58:56 +0900

    click이벤트에 따른 활성 비활성화 UI

--- src/app/interactive/three-door/three-door.component.ts
+++ src/app/interactive/three-door/three-door.component.ts
@@ -3,13 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-three-door',
   templateUrl: './three-door.component.html',
-  styleUrls: ['./three-door.component.scss']
+  styleUrls: ['./three-door.component.scss'],
 })
 export class ThreeDoorComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    const stageElem = document.querySelector('.stage');
+    let currentItem;
 
+    function doorHandle(event) {
+      const targetElem = <HTMLElement>event.target;
+      //const currentItem = document.querySelector('.door-opened');
+      if (currentItem) {
+        currentItem.classList.remove('door-opened');
+      }
+
+      if (targetElem.classList.contains('door-body')) {
+        /**클릭이벤트가 발생할때,
+         * 해당 타겟을 감싸고 있는 부모노드에
+         * 접근하여 classList 함수를 이용해 엘리먼트에 door-opend클래스를 추가한다.
+         * * */
+        (targetElem.parentNode as HTMLElement).classList.add('door-opened');
+        currentItem = targetElem.parentNode;
+      }
+    }
+
+    stageElem.addEventListener('click', doorHandle);
+  }
 }
