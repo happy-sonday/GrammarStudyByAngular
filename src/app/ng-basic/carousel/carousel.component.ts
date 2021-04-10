e853a66162d1fb4bfeb7b200e27a91d4b0acfe59
sws <sws@paywith.co.kr>
2021-04-10 19:44:11 +0900

    마우스 버튼을 뗐을때 end이벤트를 발생하기 위한 drop 옵저버블 변수 생성

--- src/app/ng-basic/carousel/carousel.component.ts
+++ src/app/ng-basic/carousel/carousel.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { fromEvent, merge, Observable } from 'rxjs';
-import { map, mergeAll, switchMap, takeUntil } from 'rxjs/operators';
+import {
+  first,
+  map,
+  mergeAll,
+  switchMap,
+  take,
+  takeUntil,
+} from 'rxjs/operators';
 
 @Component({
   selector: 'app-carousel',
@@ -29,21 +36,38 @@ export class CarouselComponent implements OnInit {
 
     /**마우스 또는 터치 시작 위치 값 */
     const start$ = fromEvent($view, EVENTS.start).pipe(toPos);
-
-    start$.subscribe(console.log);
+    //start$.subscribe(console.log);
 
     /**마우스 또는 터치 hover시 x축 방향 위치 값 확인이 가능 */
     const move$ = fromEvent($view, EVENTS.move).pipe(toPos);
-    move$.subscribe(console.log);
+    //move$.subscribe(console.log);
     const end$ = fromEvent($view, EVENTS.end);
 
     /** 해당 container 위에 마우스를 올리면 console에서 위치값을 읽는다. */
     const drag$ = start$.pipe(
       //map((start) => move$.pipe(takeUntil(end$))),
       //mergeAll()
-      switchMap((start) => move$.pipe(takeUntil(end$)))
+
+      /**마우스 버튼을 누르는 순간부터 버튼을 뗄 때까지의 마우스 이동거리 차가 데이터로 전달 */
+      switchMap((start: number) => {
+        return move$.pipe(
+          map((move: number) => move - start),
+          takeUntil(end$)
+        );
+      })
     );
 
+    drag$.subscribe((distance) =>
+      console.log('start$ 와 move$의 차이 값', distance)
+    );
+
+    /** 캐러셀에서 드래그 이후 마우스를 버튼을 떼는 행위를 드롭이라고 한다. 반드시 마우스 버튼을 딱한번 떼었을 때를 이야기한다. */
+    /**take 첫번째 인자로 전달하는 데이터의 개수를 전달받아 해당 개수째에서 상태를 종료처리하고 자동으로 구독 해제한다. 동일하게 first() 오펄이트 사용이 가능 */
+    /** 참고 직접적으로 구독을 해제(unscribe)하지 말고 takeUntil,take,first 오퍼레이터를 이용하여 자동으로 구독해제한다. */
+    //const drop$ = drag$.pipe(map((drag) => end$.pipe(take(1))));
+
+    const drop$ = drag$.pipe(switchMap((drag) => end$.pipe(first())));
+    drop$.subscribe(console.log);
     /** start와 ends 중복 map을 사용하여 자기자신을 반화하는 observable 함수를 콜백으로 사용 */
     function toPos(obs$) {
       return obs$.pipe(
