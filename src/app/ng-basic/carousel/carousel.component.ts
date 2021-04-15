36118df6de1b4fa104ac7b2c75fe019d4a5050e2
sws <sws@paywith.co.kr>
2021-04-16 06:55:06 +0900

    캐러셀 UI 상태관리하기부터 시작하기

--- src/app/ng-basic/carousel/carousel.component.ts
+++ src/app/ng-basic/carousel/carousel.component.ts
@@ -5,6 +5,7 @@ import {
   first,
   map,
   mergeAll,
+  mergeMap,
   share,
   startWith,
   switchMap,
@@ -23,7 +24,7 @@ export class CarouselComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    const $view = document.getElementById('carousel');
+    /*  const $view = document.getElementById('carousel');
     const $container = $view.querySelector('.container');
     const PANEL_COUNT = $container.querySelectorAll('.panel').length;
     const SUPPORT_TOUCH = 'ontouchstart' in window;
@@ -73,6 +74,68 @@ export class CarouselComponent implements OnInit {
     );
 
     const carousel$ = merge(drag$, drop$);
-    carousel$.subscribe((v) => console.log('캐러셀 데이터확인', v));
+    carousel$.subscribe((v) => console.log('캐러셀 데이터확인', v)); */
+
+    const $view = document.getElementById('carousel');
+    const $container = $view.querySelector('.container');
+    const PANEL_COUNT = $container.querySelectorAll('.panel').length;
+    //console.log(PANEL_COUNT);
+    const SUPPORT_TOUCH = 'ontouchstart' in window;
+    const EVENTS = {
+      start: SUPPORT_TOUCH ? 'touchstart' : 'mousedown',
+      move: SUPPORT_TOUCH ? 'touchmove' : 'mouseover',
+      end: SUPPORT_TOUCH ? 'touchend' : 'mouseup',
+    };
+
+    const start$ = fromEvent($view, EVENTS.start).pipe(toPos);
+    const move$ = fromEvent($view, EVENTS.move).pipe(toPos);
+    const end$ = fromEvent($view, EVENTS.end);
+
+    const drag$ = start$.pipe(
+      switchMap((start) => {
+        return move$.pipe(
+          map((move) => move - start),
+          takeUntil(end$)
+        );
+      }),
+
+      map((distance) => ({ distance })),
+      share()
+    );
+
+    drag$.subscribe();
+
+    const size$ = fromEvent(window, 'resize').pipe(
+      startWith(0),
+      map((event) => $view.clientWidth)
+    );
+
+    const drop$ = drag$.pipe(
+      switchMap((drag) => {
+        return end$.pipe(
+          map((event) => drag),
+          first()
+        );
+      }),
+      withLatestFrom(size$, (drag, size) => {
+        return { ...drag, size };
+      })
+    );
+
+    /* function toPos(event: any) {
+      return SUPPORT_TOUCH
+        ? event.changedTouches[0].pageX
+        : (event as MouseEvent).pageX;
+    } */
+
+    const carousel$ = merge(drag$, drop$);
+    carousel$.subscribe((v) => console.log('캐러셀 데이터', v));
+    function toPos(obj$: Observable<any>) {
+      return obj$.pipe(
+        map((v) =>
+          SUPPORT_TOUCH ? v.changedTouches[0].pageX : (v as MouseEvent).pageX
+        )
+      );
+    }
   }
 }
